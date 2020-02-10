c7302bcd5e0fe9731d8a75a5ed6293dcfaaea7cd
maneroto <maneroto@hotmail.com>
2020-02-10 15:06:02 -0600

    Modify submenu classses

--- js/submenu.js
+++ js/submenu.js
@@ -5,30 +5,40 @@ Element.prototype.submenu = function(options = {})
 		options.submenu = options.submenu || '.submenu';
 		options.displayer = options.displayer || '.submenuDisplayer';
 		options.nested = options.nested || false;
+		options.closeButton = options.closeButton || false;
 
 		return options;
 	}; options = constructor(options);
 
 	let displayers = this.querySelectorAll(options.displayer);
-	let submenus = this.querySelectorAll('.nav .submenu');
+	let submenus = this.querySelectorAll(options.submenu);
 
 	for (let i = 0; i < displayers.length; i ++) displayers[i].addEventListener('click', function(e)
 	{
-		let currentSubmenu = this.parentElement.querySelector('.submenu');
+		let currentSubmenu = this.parentElement.querySelector(options.submenu);
 
 		if (options.nested) 
 		{
 			submenus = this.parentElement.dataset.layer;
-			submenus = document.querySelectorAll('[data-layer="'+submenus+'"] > .submenu');
+			submenus = document.querySelectorAll('[data-layer="' + submenus + '"] > ' + options.submenu);
 		}
 
-		if(e.target == this)
+		if(e.target == this || (options.closeButton && e.target == closeButton))
 		{
-			submenus.forEach(sub => console.log(sub.classList));
 			for(let i = 0; i < submenus.length; i ++) if (submenus[i] != currentSubmenu) submenus[i].classList.remove('active');
 			currentSubmenu.classList.toggle('active');
 		}
 	});
+
+	if (options.closeButton)
+	{
+		let buttons = document.querySelectorAll(options.closeButton);
+		for (let i = 0; i < buttons.length; i ++) buttons[i].addEventListener('click', function(e)
+		{
+			let currentSubmenu = this.parentElement;
+			currentSubmenu.classList.remove('active');
+		});
+	}
 }
 
 NodeList.prototype.submenu = HTMLCollection.prototype.submenu = function(options) {
